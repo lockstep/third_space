7ef4c959a787bc747b8ed49e4d2fa45f3eb2bb6e
GotZilaZa <kirati@tripler.co.th>
2017-02-09 11:37:16 +0700

    removed 'What is your expected result?'

--- app/models/input.rb
+++ app/models/input.rb
@@ -1,7 +1,7 @@
 class Input < ApplicationRecord
   belongs_to :problem
   LENSES = %w(adaptability cultural_competence empathy intellectual_curiosity thinking)
-  TYPES = %w(solution result summary)
+  TYPES = %w(solution summary)
 
   def self.next_lens(current_lens)
     return LENSES.last if LENSES.last == current_lens
