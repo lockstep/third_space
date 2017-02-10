9c4254d006c1fe2807b89364fe3b577e33a5918e
gunosk129 <gunosk129@users.noreply.github.com>
2017-02-10 15:12:20 +0700

    updated discuss ui
    * Add discuss view and spec
    
    * Refactor css to be BEM style
    
    * fixup! Refactor css to be BEM style

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   resources :problems, only: [:index, :new, :update, :edit] do
-    get ":id/discuss", to: 'problems#discuss', on: :collection
+    get ":id/discuss", to: 'problems#discuss', on: :collection, as: :discuss
     get ":id/:lens", to: 'problems#show', on: :collection, as: :view
   end
 
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
     get ":problem_id/:lens/:input_type", to: 'inputs#input_text', on: :collection, as: :input_text
     get ":problem_id/count", to: 'inputs#count', on: :collection, as: :input_count
   end
-
+  
+  resources :comments, only: [:create]
 end
