60d9bb0b6ab0ef6430ac88222fd9c9fbb1440842
gotzilaza <kirati.samat@gmail.com>
2017-02-10 11:28:23 +0700

    comment controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+# class CommentsController < ApplicationController
+#   def create
+#     comment = Comment.new(comment_params)
+#     # if comment.save
+#     #   redirect_to duscuss_problems()
+#     # else
+#     #   #
+#     # end
+#   end
+#
+#   private
+#
+#   def comment_params
+#     params.require(:comment).permit(:problem_id, :user_id, :description)
+#   end
+# end
