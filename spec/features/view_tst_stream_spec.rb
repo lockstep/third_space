0b352c39e05334e55ebe85bb65240d3b6912ab73
gotzilaza <kirati.samat@gmail.com>
2017-02-15 15:20:17 +0700

    showed my/all problem

--- spec/features/view_tst_stream_spec.rb
+++ spec/features/view_tst_stream_spec.rb
@@ -6,6 +6,27 @@ feature 'View TST Stream' do
     login_as(@user, scope: :user)
   end
 
+  context 'TST Stream' do
+    background do
+      create(:problem, name: 'my problem', user: @user)
+      create(:problem, name: "Somebody else's problem", user: create(:user))
+    end
+
+    scenario 'views my problem' do
+      visit problems_path
+      click_link 'My'
+      expect(page).to have_content 'my problem'
+      expect(page).to_not have_content "Somebody else's problem"
+    end
+
+    scenario 'views all problem' do
+      visit problems_path
+      click_link 'All'
+      expect(page).to have_content 'my problem'
+      expect(page).to have_content "Somebody else's problem"
+    end
+  end
+
   scenario 'renders pagination correctly' do
     create_list(:problem, 30, user: @user)
     create(:problem, name: 'On second page', user: @user)
