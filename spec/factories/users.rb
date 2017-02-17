22ebc4204fa1743baccf0a26885d211de7fba480
gotzilaza <kirati.samat@gmail.com>
2017-02-17 12:07:59 +0700

    added first&last name to user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "person#{n}@example.com" }
+    first_name 'donale'
+    last_name 'trump'
     password 'password'
     role 'user'
   end
