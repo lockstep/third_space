544d8108c3f4465c54ee209930de1ece25449581
GotZilaZa <kirati@tripler.co.th>
2017-02-21 13:22:24 +0700

    ordered latest problem in TST stream page

--- spec/features/view_tst_stream_spec.rb
+++ spec/features/view_tst_stream_spec.rb
@@ -6,37 +6,62 @@ feature 'View TST Stream' do
     login_as(@user, scope: :user)
   end
 
-  context 'TST Stream' do
+  context 'TST stream' do
     background do
-      create(:problem, name: 'my problem', user: @user)
-      create(:problem, name: "Somebody else's problem", user: create(:user))
+      create(:problem, name: 'my problem', user: @user, created_at: Date.parse('/06/08/2015'))
+      create(:problem, name: 'my problem 2', user: @user, created_at: Date.parse('/07/08/2015'))
+      create(:problem, name: "Somebody else's problem", user: create(:user), created_at: Date.parse('/08/08/2015'))
+      create(:problem, name: "Somebody else's problem 2", user: create(:user), created_at: Date.parse('/09/08/2015'))
     end
 
-    scenario 'views my problem' do
-      visit problems_path
-      click_link 'My'
-      expect(page).to have_content 'my problem'
-      expect(page).to_not have_content "Somebody else's problem"
+    context 'My problem' do
+      scenario 'displays my problems' do
+        visit problems_path
+        click_link 'My'
+        expect(page).to have_content 'my problem'
+        expect(page).to have_content 'my problem 2'
+        expect(page).to_not have_content "Somebody else's problem"
+        expect(page).to_not have_content "Somebody else's problem 2"
+      end
+
+      scenario 'displays latest problems first' do
+        visit problems_path
+        click_link 'My'
+        expect(all('.problem__title')[0].text).to eq 'my problem 2'
+        expect(all('.problem__title')[1].text).to eq 'my problem'
+      end
     end
 
-    scenario 'views all problem' do
-      visit problems_path
-      click_link 'All'
-      expect(page).to have_content 'my problem'
-      expect(page).to have_content "Somebody else's problem"
+    context 'All problem' do
+      scenario 'views all problems' do
+        visit problems_path
+        click_link 'All'
+        expect(page).to have_content 'my problem'
+        expect(page).to have_content 'my problem 2'
+        expect(page).to have_content "Somebody else's problem"
+        expect(page).to have_content "Somebody else's problem 2"
+      end
+
+      scenario 'displays latest problems first' do
+        visit problems_path
+        click_link 'All'
+        expect(all('.problem__title')[0].text).to eq "Somebody else's problem 2"
+        expect(all('.problem__title')[1].text).to eq "Somebody else's problem"
+        expect(all('.problem__title')[2].text).to eq 'my problem 2'
+        expect(all('.problem__title')[3].text).to eq 'my problem'
+      end
     end
   end
 
   scenario 'renders pagination correctly' do
-    create_list(:problem, 30, user: @user)
-    create(:problem, name: 'On second page', user: @user)
-
+    create_list(:problem, 31, user: @user)
+    Problem.first.update(name: 'first problem')
     visit problems_path
-    expect(page).to have_content('My Problem')
-    expect(page).not_to have_content("On second page")
-
+    click_link 'My'
+    expect(page).to have_content('My Problem 31')
+    expect(page).not_to have_content('first problem')
     click_on '2'
-    expect(page).to have_content("On second page")
-    expect(page).not_to have_content('My Problem')
+    expect(page).to have_content('first problem')
+    expect(page).to_not have_content('My Problem 31')
   end
 end
