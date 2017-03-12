478b8ed7406d2609b0564f9b0d6fc93977bcd84b
Kirati Samattanares <kirati.samat@gmail.com>
2017-03-12 18:14:47 +0700

    tried another way to fix email image problem

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,19 @@
 class UserMailer < ApplicationMailer
+  before_action :attach_header_image
+
   def problem_with_solution(problem_id, receiver_email)
     @problem = Problem.find(problem_id)
     mail(to: receiver_email,
       bcc: ['shellee.smith@usc.edu', 'paul@locksteplabs.com'],
       subject: 'New Problem With Solution'
+    )
+  end
+
+  private
+
+  def attach_header_image
+    attachments.inline['thirdspace_header_email.png'] = File.read(
+      "#{Rails.root}/app/assets/images/thirdspace_header_email.png"
     )
   end
 end
