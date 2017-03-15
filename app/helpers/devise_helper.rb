09a07aee363e0cb716bcab45a692089435be2041
Kantaphon Kengthong <kantaphon@tripler.co.th>
2017-03-15 16:25:17 +0700

    Fix change password page

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,7 +1,7 @@
 module DeviseHelper
   def devise_error_messages!
     return "" unless devise_error_messages?
-    resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join.html_safe
+    resource.errors.full_messages.map { |msg| content_tag(:div, msg, class: 'error') }.join.html_safe
   end
 
   def devise_error_messages?
